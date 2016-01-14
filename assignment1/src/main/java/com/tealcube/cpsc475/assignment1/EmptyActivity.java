8ff532400c661439ef087a0302b9b71a7ce3721f
Richard Harrah <richard.harrah2@anthem.com>
2016-01-14 17:28:06 -0500

    Initial commit

---
+++ assignment1/src/main/java/com/tealcube/cpsc475/assignment1/EmptyActivity.java
@@ -0,0 +1,12 @@
+package com.tealcube.cpsc475.assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmptyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
